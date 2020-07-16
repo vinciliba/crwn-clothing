8ac489925405752313f21c6d5a18e14953ba6712
vinciliba <39335992+vinciliba@users.noreply.github.com>
2020-07-16 16:43:36 +0200

    removing redux-logger from production build

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import './directory.styles.scss';
-import MenuItem from '../menu-item/menu-item.component.jsx';
 import { connect } from 'react-redux';
-import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 import { createStructuredSelector } from 'reselect';
 
-const Directory = ({ sections }) => {
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
-return(
-<div className="directory-menu">
-        {sections.map(({id,...sectionProps}) => {
-         return <MenuItem key = {id} {...sectionProps} />
-        })   
-       }     
-    </div>
+import MenuItem from '../menu-item/menu-item.component';
 
+import './directory.styles.scss';
+
+const Directory = ({ sections }) => (
+  <div className='directory-menu'>
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
 );
-}
 
-const mapStateToProps = createStructuredSelector ({
-    sections: selectDirectorySections
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
 });
-  
+
 export default connect(mapStateToProps)(Directory);
